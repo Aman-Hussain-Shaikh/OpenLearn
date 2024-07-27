2dcd29d5fe75bc25621dbe957ca7ad4b42db8872
Aman-Hussain-Shaikh <theamazingpeace@gmail.com>
2024-07-27 21:11:58 +0530

    tailwindconfig.ts fixed

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -6,12 +6,12 @@ const Footer = () => {
   return (
     <div>
       <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
-        < Image src={assets.logo_light } alt='' width={120}/>
+        <Image src={assets.logo_light } alt='' width={120}/>
         <p className='text-sm text-white'>All rights reserved. Copyright</p>
         <div className='flex'>
-        < Image src={assets.linkedin} alt='' className='w-8 h-8 mx-1 cursor-pointer'/>
-        < Image src={assets.instagram } alt='' className='w-8 h-8  mx-1 cursor-pointer'/>
-        < Image src={assets.telegram } alt='' className='w-8 h-8  mx-1 cursor-pointer'/>
+        <Image src={assets.linkedin} alt='' className='w-8 h-8 mx-1 cursor-pointer'/>
+        <Image src={assets.instagram } alt='' className='w-8 h-8  mx-1 cursor-pointer'/>
+        <Image src={assets.telegram } alt='' className='w-8 h-8  mx-1 cursor-pointer'/>
         </div>
       </div>
     
@@ -20,3 +20,4 @@ const Footer = () => {
 }
 
 export default Footer
+ 
